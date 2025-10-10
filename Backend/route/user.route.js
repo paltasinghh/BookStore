fb6a514db5a55f61b7d432b54ecdd4da598dcb5d
TestHabitatPlush <hptestplatform@gmail.com>
2025-10-10 17:29:50 +0530

    Updated frontend components

--- Backend/route/user.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { signup, login } from "../Controller/user.controller.js";
-
-const router = express.Router();
-
-router.post("/signup", signup);  // Pass the signup function here
-router.post("/login", login);
-
-export default router;
